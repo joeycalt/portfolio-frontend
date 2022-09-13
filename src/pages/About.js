470b382d638f8af579d53bb4d5ca2c9d0e172b9f
joeycalt <joeycalt@yahoo.com>
2022-09-13 16:17:27 -0400

    made the wrong fix, came back to it and made the right one

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,8 +19,8 @@ function About(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-      getAboutData(props.URL)
-    }, []);
+      getAboutData()
+    }, [props.URL]);
     
 
   // define a function that will return the JSX needed once we get the data
