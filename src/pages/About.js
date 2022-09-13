b4cc2c968a27041f14c6f378d1f7b36a90d105a3
joeycalt <joeycalt@yahoo.com>
2022-09-13 16:12:44 -0400

    fixed warnings for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,7 +19,7 @@ function About(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-      getAboutData()
+      getAboutData(props.URL)
     }, []);
     
 
