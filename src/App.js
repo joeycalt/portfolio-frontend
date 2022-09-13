b452ecde0d7e8d6403132796bbc0e5f6fab77124
joeycalt <joeycalt@yahoo.com>
2022-09-13 12:21:55 -0400

    frontend complete

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Switch } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+  const URL = "https://portfolio-practice-joeycalt.herokuapp.com/";
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects URL={URL} />} />
+        <Route path="/about" element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
